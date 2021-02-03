c67660338c52adfdd6809cf8caee8260ecf2f88f
Rainbow <70801324+RainbowPyPrograms@users.noreply.github.com>
2021-02-03 12:23:34 +0530

    main file just got updated lulw
    nothing much changed lul

--- bot.py
+++ bot.py
@@ -1,28 +1,16 @@
 import discord
 from discord.ext import commands
 import os
-import json
 import jishaku
-import random
-from datetime import datetime
-import asyncio
-import humanize
-
-class CustomEmbed(discord.Embed):
-
-
-    def __init__(self, *a, **kw):
-        super().__init__(color=0x2F3136, *a, **kw)
-
-load_time = datetime.utcnow()
+from asyncdagpi import Client
 
 async def get_prefix(client, message):
     if message.guild is None:
-        prefixes = ["Rainbow ", "hi ", "rainbow "]
+        prefixes = ["Rainbow ", "rr.", "rainbow "]
     elif message.author.id == 688293803613880334:
-        prefixes = ["Rainbow ", "hi ", "rainbow ", ""]
+        prefixes = ["Rainbow ", "rr.", "rainbow ", ""]
     else:
-        prefixes = ["Rainbow ", "hi ", "rainbow "]
+        prefixes = ["Rainbow ", "rr.", "rainbow "]
 
     return commands.when_mentioned_or(*prefixes)(client, message)
 
@@ -30,15 +18,13 @@ intents = discord.Intents.all()
 
 client = commands.Bot(command_prefix=get_prefix, case_insensitive=True,description='This is a multi-purpose bot for servers with fun commands', intents=intents)
 
-def callable_pref(bot, message):
-  ## so stuff to load your prefixes
-  # where `prefix_return` is a string or list of strings
-  # in my case it's this:
-    prefix_return = ["rr."]
-    return commands.when_mentioned_or(bot.prefixes[message.guild.id])(bot, message)
-
+client.dagpi = Client("")
+# if you want to use dagpi api put your token in the "" else just remove line 21
 jsk = client.load_extension("jishaku")
-
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+os.environ["JISHAKU_HIDE"] = "True"
+jsk = client.reload_extension("jishaku")
 @client.event
 async def on_ready():
     print(f'Main file Opened by bot consists basics.')
@@ -64,16 +50,9 @@ async def reload(ctx, extension):
     message = ctx.message
     await message.add_reaction('<:greenTick:596576670815879169>')
 
-@reload.error
-async def r_handler(ctx, error):
-    message = ctx.message
-    await message.remove_reaction('<:greenTick:596576670815879169>', ctx.guild.me)
-    await message.add_reaction('<:redTick:596576672149667840>')
-    await ctx.send(embed=discord.Embed(title='Exception caught:', description=f'{error}'))
-
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-x = input('Token:')
-client.run(x)
+
+client.run('')
+# your token 
